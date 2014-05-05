3a85c9534116d3e4c3ea284ef94147d5404a7794
Johniel <johniel.s.m@gmail.com>
2014-05-05 21:58:34 +0900

    srm619 easy

---
+++ src/SRM619/SplitStoneGame.cpp
@@ -0,0 +1,75 @@
+#include <bits/stdc++.h>
+
+#define each(i, c) for (auto& i : c)
+#define unless(cond) if (!(cond))
+
+using namespace std;
+
+typedef long long int lli;
+typedef unsigned long long ull;
+typedef complex<double> point;
+
+const int M = 50 + 5;
+int memo[M][M];
+
+const int L = 0;
+const int W = 1;
+
+int rec(int one, int other)
+{
+  int& ret = memo[one][other];
+  if (ret != -1) return ret;
+  if (other == 0) return L;
+  if (other + one <= 2) return L;
+
+  int opt = L;
+
+  if (1 <= one && 2 <= other) {
+    opt = max(opt, rec(one - 1, other) ^ 1);
+  }
+
+  if (2 <= one) {
+    opt = max(opt, rec(one - 2, other + 1) ^ 1);
+  }
+
+  if (3 <= other) {
+    opt = max(opt, rec(one, other - 1) ^ 1);
+  }
+
+  return ret = opt;
+}
+
+class SplitStoneGame {
+public:
+  string winOrLose(vector <int> v)
+  {
+    fill(&memo[0][0], &memo[M - 1][M - 1] + 1, -1);
+    int one = count(v.begin(), v.end(), 1);
+    return (rec(one, v.size() - one) == W) ? "WIN" : "LOSE";
+  }
+
+// BEGIN CUT HERE
+	public:
+	void run_test(int Case) { if ((Case == -1) || (Case == 0)) test_case_0(); if ((Case == -1) || (Case == 1)) test_case_1(); if ((Case == -1) || (Case == 2)) test_case_2(); if ((Case == -1) || (Case == 3)) test_case_3(); if ((Case == -1) || (Case == 4)) test_case_4(); }
+	private:
+	template <typename T> string print_array(const vector<T> &V) { ostringstream os; os << "{ "; for (typename vector<T>::const_iterator iter = V.begin(); iter != V.end(); ++iter) os << '\"' << *iter << "\","; os << " }"; return os.str(); }
+	void verify_case(int Case, const string &Expected, const string &Received) { cerr << "Test Case #" << Case << "..."; if (Expected == Received) cerr << "PASSED" << endl; else { cerr << "FAILED" << endl; cerr << "\tExpected: \"" << Expected << '\"' << endl; cerr << "\tReceived: \"" << Received << '\"' << endl; } }
+	void test_case_0() { int Arr0[] = {1, 1, 1}; vector <int> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); string Arg1 = "LOSE"; verify_case(0, Arg1, winOrLose(Arg0)); }
+	void test_case_1() { int Arr0[] = {2, 2}; vector <int> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); string Arg1 = "LOSE"; verify_case(1, Arg1, winOrLose(Arg0)); }
+	void test_case_2() { int Arr0[] = {1, 1, 2}; vector <int> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); string Arg1 = "WIN"; verify_case(2, Arg1, winOrLose(Arg0)); }
+	void test_case_3() { int Arr0[] = {1, 2, 3, 4, 3, 2, 2, 4, 3, 1, 4, 4, 1, 2, 4, 4, 1, 4, 3, 1, 4, 2, 1}; vector <int> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); string Arg1 = "WIN"; verify_case(3, Arg1, winOrLose(Arg0)); }
+	void test_case_4() { int Arr0[] = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 3, 1, 9, 1, 3, 1, 1, 1, 1, 1}; vector <int> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); string Arg1 = "LOSE"; verify_case(4, Arg1, winOrLose(Arg0)); }
+
+// END CUT HERE
+
+};
+
+
+// BEGIN CUT HERE
+
+int main() {
+  SplitStoneGame ___test;
+  ___test.run_test(-1);
+}
+
+// END CUT HERE
